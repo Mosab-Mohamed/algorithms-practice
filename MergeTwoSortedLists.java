fd1ef5fbfd40562b09e4a25b911b3eb602b15c3a
Mosab-Mohamed <mosab.muhammad.kh@gmail.com>
2019-07-15 00:01:32 +0200

    https://leetcode.com/problems/merge-two-sorted-lists

---
+++ MergeTwoSortedLists.java
@@ -0,0 +1,30 @@
+// https://leetcode.com/problems/merge-two-sorted-lists
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode merge(ListNode l1, ListNode l2) {
+		if(l1 == null) return l2;
+		if(l2 == null) return l1;
+
+		ListNode l;
+		if(l1.val < l2.val) {
+			l = l1;
+			l.next = merge(l1.next, l2);
+		} else {
+			l = l2;
+			l.next = merge(l1, l2.next);
+		}
+		return l;
+	}
+    
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        return merge(l1, l2);
+    }
+}
