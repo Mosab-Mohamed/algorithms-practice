fe5bc2a3f4e723e2d584589ca94cb8df6fed6065
Mosab-Mohamed <mosab.muhammad.kh@gmail.com>
2019-07-29 02:06:43 +0200

    Update first-unique-character-in-a-string.java

--- amazon-questions/first-unique-character-in-a-string.java
+++ amazon-questions/first-unique-character-in-a-string.java
@@ -1,1 +1,17 @@
 // https://leetcode.com/problems/first-unique-character-in-a-string
+
+class Solution {
+    public int firstUniqChar(String s) {
+        HashMap<Character, Boolean> map = new HashMap<Character, Boolean>();
+        for(int i=0; i<s.length(); i++){
+            if(map.containsKey(s.charAt(i))) {
+                map.put(s.charAt(i), false);
+            }
+            else map.put(s.charAt(i), true);
+        }
+        for(int i=0; i<s.length(); i++){
+            if(map.containsKey(s.charAt(i)) && map.get(s.charAt(i))) return i;
+        }
+        return -1;
+    }
+}
